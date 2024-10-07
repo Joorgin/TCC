e68c455edf6dcfe9f973874881a669fba101032e
Joorgin <88665411+Joorgin@users.noreply.github.com>
2024-10-07 10:59:44 -0300

    Stamina e boss UPdate

--- tcc/Assets/Script/Enemys/Health/EnemyHeatlh.cs
+++ tcc/Assets/Script/Enemys/Health/EnemyHeatlh.cs
@@ -18,6 +18,10 @@ public class EnemyHeatlh : MonoBehaviour
     // Onde se encaixa o flashSprite
     public FleashMaterial fleashMaterialScript;
 
+    // Quanto de stamina o inimigo consede
+    [Space]
+    public float stamina;
+
     private void Awake()
     {
         m_List.Add(this);
@@ -77,7 +81,7 @@ public class EnemyHeatlh : MonoBehaviour
     {
         Contador_de_Almas.instance.AlmentarAlmas(1);
         Instantiate(Bracelete, gameObject.transform.position, Quaternion.identity);
-        //dá find e tira da lista
+        Stamina.UpStamina(stamina);
         Destroy(gameObject);
     }
 
--- tcc/Assets/Script/Player/Health/Stamina.cs
+++ tcc/Assets/Script/Player/Health/Stamina.cs
@@ -6,7 +6,7 @@ public class Stamina : MonoBehaviour
 {
     // Stamina do Player
     public static float stamina;
-    float staminaStay;
+    public static float staminaStay;
     // Ui Da Stamina
     public Stamina_UI staminaUI;
     private void Start()
@@ -27,5 +27,10 @@ public class Stamina : MonoBehaviour
             PlayerHealth.deadByStamina = true;
             staminaStay = stamina;
         }
+    }
+
+    public static void UpStamina(float stamina)
+    {
+        staminaStay += stamina;
     }
 }
