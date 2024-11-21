85cf584747402eb07217f12e60398fdd5900e17b
Felipe Moraes <89394365+cavernadormaster@users.noreply.github.com>
2024-11-21 08:35:09 -0300

    Update de carmos

--- tcc/Assets/Script/Player/Health/Stamina.cs
+++ tcc/Assets/Script/Player/Health/Stamina.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Stamina : MonoBehaviour
 {
     // Stamina do Player
-    public static float stamina;
+    public static float estamina;
     public static float staminaStay;
     // Ui Da Stamina
     public Stamina_UI staminaUI;
@@ -18,8 +18,8 @@ public class Stamina : MonoBehaviour
     private void Awake()
     {
         instance = this;
-        stamina = GameManager.PlayerStamina;
-        staminaStay = stamina;
+        estamina = GameManager.PlayerStamina;
+        staminaStay = estamina;
         staminaUI.SetMaxStamina(staminaStay);
     }
     void Update()
@@ -54,11 +54,12 @@ public class Stamina : MonoBehaviour
     public void UpStamina(float stamina)
     {
         staminaStay += stamina;
+        if (staminaStay > estamina) staminaStay = estamina;
     }
 
     public void SetMaxStainaAfterSceneChange()
     {
-        staminaStay = stamina;
+        staminaStay = estamina;
         staminaUI.SetStamina(staminaStay);
     }
 }
